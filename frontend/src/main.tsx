e58621520781674208ac59a1d0ab1807bdf37bd7
yashitrana49-cmyk <yashitrana49@gmail.com>
2026-03-26 00:45:13 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import{ BrowserRouter } from "react-router-dom"
-import { StrictMode } from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 import { CartProvider } from "./context/CartContext";
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <BrowserRouter>
-    <CartProvider>
-    <App />
-    </CartProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
-    </StrictMode>
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
