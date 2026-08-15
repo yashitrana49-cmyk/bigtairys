798511b87bb6f4051ff253842f3d96435cf6d94c
Yashit Rana <yashitrana49@gmail.com>
2026-08-15 20:12:08 +0530

    Fix LaserFlow build

--- frontend/src/components/LaserFlow.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*LaserFlow' {
-  import React from 'react';
-  const LaserFlow: React.FC<any>;
-  export default LaserFlow;
-}
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import LaserFlow from '../components/LaserFlow';
+import { LaserFlow } from '../components/LaserFlow';
 
 export default function MainLayout() {
   return (
@@ -16,6 +16,7 @@ export default function MainLayout() {
         zIndex: 0,
         backgroundColor: '#000'
       }}>
+        {/* @ts-expect-error LaserFlow provides defaults for all props internally */}
         <LaserFlow />
       </div>
 
